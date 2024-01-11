c051190bdc5f1b2a381a641224a499f176817bda
Dario Caric <dario.caric@gmail.com>
2024-01-12 00:21:34 +0100

    first commit

---
+++ linkedList/Node.cpp
@@ -0,0 +1,8 @@
+//
+//  Node.cpp
+//  linkedList
+//
+//  Created by Dario Caric on 11/01/2024.
+//
+
+#include "Node.hpp"
---
+++ linkedList/Node.hpp
@@ -0,0 +1,13 @@
+//
+//  Node.hpp
+//  linkedList
+//
+//  Created by Dario Caric on 11/01/2024.
+//
+
+#ifndef Node_hpp
+#define Node_hpp
+
+#include <stdio.h>
+
+#endif /* Node_hpp */
