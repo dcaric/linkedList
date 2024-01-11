eef213fd7b3f8f78f6d5d971401df9ae13242945
Dario Caric <dario.caric@gmail.com>
2024-01-11 21:42:10 +0100

    Initial Commit

---
+++ linkedList/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  linkedList
+//
+//  Created by Dario Caric on 11/01/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
